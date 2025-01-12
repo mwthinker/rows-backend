ab0945af0861934b7637604d28bca3f13d5e65db
Marcus Welander <mwthinker@outlook.com>
2025-01-12 14:01:38 +0100

    First commit

---
+++ src/main/java/se/mwthinker/rows_backend/RowsBackendApplication.java
@@ -0,0 +1,13 @@
+package se.mwthinker.rows_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RowsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/mwthinker/rows_backend/RowsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.mwthinker.rows_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
