1a367669a2ab2b385d9bb5c0ceb02321c0dd7aad
Anders Larsson <anders@sazu.se>
2025-02-04 19:01:08 +0100

    Moving some client code to components

---
+++ client/src/components/Board.tsx
@@ -0,0 +1,27 @@
+import BoardCell from './BoardCell';
+
+interface propTypes {
+  board: string[][], 
+  gridSize: number, 
+  cellSize: number, 
+  handleClick: CallableFunction
+}
+
+function Board({board, gridSize, cellSize, handleClick}: propTypes) {
+  return (
+    <div className={`grid gap-0`}
+      style={{
+        gridTemplateColumns: `repeat(${gridSize}, ${cellSize}px)`,
+        gridTemplateRows: `repeat(${gridSize}, ${cellSize}px)`,
+      }}
+    >
+      {board.map((row, rowIndex) =>
+        row.map((cell, colIndex) => (
+          <BoardCell key={`${rowIndex}-${colIndex}`} cell={cell} colIndex={colIndex} rowIndex={rowIndex} handleClick={handleClick} />
+        ))
+      )}
+    </div>
+  )
+}
+
+export default Board;
---
+++ client/src/components/GameEndResult.tsx
@@ -0,0 +1,21 @@
+interface propTypes {
+    winner: string | null,
+    resetGame: CallableFunction
+}
+
+function GameEndResult({winner, resetGame}: propTypes) {
+
+    return <>
+      {winner && (
+        <div className={`absolute bg-black p-5 w-full text-center content-center h-[35svh] m-auto top-0 bottom-0`}>
+          <h2>Player {winner} Wins!</h2>
+          <button className={`button mb-5`} onClick={resetGame}>
+            New Game
+          </button>
+        </div>
+      )}
+    </>
+    
+}
+
+export default GameEndResult;
