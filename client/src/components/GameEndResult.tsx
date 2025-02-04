bad9f964f2d7decceefa45373f761df8b458da56
Anders Larsson <anders@sazu.se>
2025-02-04 19:04:25 +0100

    Adding fix to function type in game end component

--- client/src/components/GameEndResult.tsx
+++ client/src/components/GameEndResult.tsx
@@ -9,7 +9,7 @@ function GameEndResult({winner, resetGame}: propTypes) {
       {winner && (
         <div className={`absolute bg-black p-5 w-full text-center content-center h-[35svh] m-auto top-0 bottom-0`}>
           <h2>Player {winner} Wins!</h2>
-          <button className={`button mb-5`} onClick={resetGame}>
+          <button className={`button mb-5`} onClick={() => {resetGame()}}>
             New Game
           </button>
         </div>
